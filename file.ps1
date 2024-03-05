5798bad2d48170884dd2518bdb547a5e651cb684
cxumol <cxumol@users.noreply.github.com>
2024-03-04 21:33:48 -0800

    Update file.ps1

--- file.ps1
+++ file.ps1
@@ -1,6 +1,5 @@
-## rename all pngs into %04d.png
+## rename all pngs into %04d.png and keep order
 $files = Get-ChildItem -Filter "*.png" | Sort-Object
-
 for ($i = 0; $i -lt $files.Count; $i++) {
     $newName = "{0:D4}.png" -f ($i + 1)
     Rename-Item -Path $files[$i].FullName -NewName $newName
