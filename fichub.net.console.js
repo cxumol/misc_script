3f3f7f10bf709b62ab5e6eb8131306654b26310b
cxumol <cxumol@users.noreply.github.com>
2024-09-01 02:44:34 -0700

    add fichubToWget(url)

--- fichub.net.console.js
+++ fichub.net.console.js
@@ -6,3 +6,15 @@ function generateWgetFichub(url) {
 
 var url = '' // Download as EPUB @ https://fichub.net/
 copy(generateWgetFichub(url))
+
+/* no open tab */
+
+async function fichubToWget(url) {
+    const data = await fetch(`https://fichub.net/api/v0/epub?q=${encodeURIComponent(url)}`).then(r=>r.json());
+    const fullEpubUrl = `https://fichub.net${data.epub_url}`;
+    const baseFileName = new URL(fullEpubUrl).pathname.split('/').pop().split('-').slice(0, -1).join('-') + '.epub';
+    const ans=`wget "${fullEpubUrl}" -O "${baseFileName}"`;
+    return ans;
+}
+var url = '';
+fichubToWget(url).then(cmd => copy(cmd));
