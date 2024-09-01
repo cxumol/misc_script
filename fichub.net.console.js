0e7ac545ac0806e2b471e8f1db0d66000f4a6ff3
cxumol <cxumol@users.noreply.github.com>
2024-09-01 03:07:26 -0700

    Update fichub.net.console.js

--- fichub.net.console.js
+++ fichub.net.console.js
@@ -10,11 +10,23 @@ copy(generateWgetFichub(url))
 /* no open tab */
 
 async function fichubToWget(url) {
-    const data = await fetch(`https://fichub.net/api/v0/epub?q=${encodeURIComponent(url)}`).then(r=>r.json());
-    const fullEpubUrl = `https://fichub.net${data.epub_url}`;
-    const baseFileName = new URL(fullEpubUrl).pathname.split('/').pop().split('-').slice(0, -1).join('-') + '.epub';
-    const ans=`wget "${fullEpubUrl}" -O "${baseFileName}"`;
-    return ans;
+  const data = await fetch(`https://fichub.net/api/v0/epub?q=${encodeURIComponent(url)}`).then(r=>r.json());
+  const fullEpubUrl = `https://fichub.net${data.epub_url}`;
+  const baseFileName = new URL(fullEpubUrl).pathname.split('/').pop().split('-').slice(0, -1).join('-') + '.epub';
+  const ans=`wget "${fullEpubUrl}" -O "${baseFileName}"`;
+  console.log(nas);
+  return ans;
 }
+var url = '';
+fichubToWget(url).then(cmd => copy(cmd));
+
+/* miniam w/ async/await */
+
+var fichubToWget = url => fetch(`https://fichub.net/api/v0/epub?q=${encodeURIComponent(url)}`).then(r => r.json()).then(data => {
+  var fullEpubUrl = `https://fichub.net${data.epub_url}`;
+  var baseFileName = new URL(fullEpubUrl).pathname.split('/').pop().split('-').slice(0, -1).join('-') + '.epub';
+  var ans = `wget "${fullEpubUrl}" -O "${baseFileName}"`;
+  console.log(ans);return ans;
+});
 var url = '';
 fichubToWget(url).then(cmd => copy(cmd));
