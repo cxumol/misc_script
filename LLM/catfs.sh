0bb740fe6d38a5498af7d1f5a2615848f665bbd4
cxumol <cxumol@users.noreply.github.com>
2025-08-11 20:16:57 -0700

    Update catfs.sh

--- LLM/catfs.sh
+++ LLM/catfs.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # example: curl -sL https://github.com/cxumol/misc_script/raw/refs/heads/master/LLM/catfs.sh | bash -s -- 'src/**/*.py'
 set -e
-OUTPUT_PATH = catfs.txt
+OUTPUT_PATH="catfs.txt"
 {
   for file in "$@"; do
     if [ -f "$file" ]; then
